2762fb03804ec6a35b49b24b619a991922046413
erichuytran <erictran@live.fr>
2020-10-07 16:23:22 +0200

    Exercice 101 done

--- src/main/java/com/ipiecoles/java/java220/Employe.java
+++ src/main/java/com/ipiecoles/java/java220/Employe.java
@@ -1,9 +1,60 @@
 package com.ipiecoles.java.java220;
 
+import org.joda.time.LocalDate;
+
 /**
  * Created by pjvilloud on 21/09/17.
  */
 
 public class Employe {
-    
+    private String nom = "";
+    private String prenom = "";
+    private String matricule = "";
+    private LocalDate dateEmbauche;
+    private Double salaire = 0.0;
+
+    //nom
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    //prenom
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    //matricule
+    public String getMatricule() {
+        return matricule;
+    }
+
+    public void setMatricule(String matricule) {
+        this.matricule = matricule;
+    }
+
+    //dateEmbauche
+    public LocalDate getDateEmbauche() {
+        return dateEmbauche;
+    }
+
+    public void setDateEmbauche(LocalDate dateEmbauche) {
+        this.dateEmbauche = dateEmbauche;
+    }
+
+    //salaire
+    public Double getSalaire() {
+        return salaire;
+    }
+
+    public void setSalaire(Double salaire) {
+        this.salaire = salaire;
+    }
 }
