b90a1b8830a9dbc296149845f596b643d2cfe8c9
erichuytran <erictran@live.fr>
2020-10-08 14:40:46 +0200

    303 Done

--- src/main/java/com/ipiecoles/java/java220/Technicien.java
+++ src/main/java/com/ipiecoles/java/java220/Technicien.java
@@ -3,12 +3,13 @@ package com.ipiecoles.java.java220;
 import org.joda.time.LocalDate;
 
 public class Technicien extends Employe {
-    Integer grade = 0;
+    private Integer grade = 0;
 
     public Technicien() { }
 
-    public Technicien(String nom, String prenom, String matricule, LocalDate dateEmbauche, Double salaire) {
+    public Technicien(String nom, String prenom, String matricule, LocalDate dateEmbauche, Double salaire, Integer grade) {
         super(nom, prenom, matricule, dateEmbauche, salaire);
+        this.grade = grade;
     }
 
     public Integer getGrade() {
