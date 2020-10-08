53653e32693cf54c1a8a64cf0474391bcac248e1
erichuytran <erictran@live.fr>
2020-10-08 12:03:48 +0200

    203 Done

--- src/main/java/com/ipiecoles/java/java220/Commercial.java
+++ src/main/java/com/ipiecoles/java/java220/Commercial.java
@@ -14,4 +14,14 @@ public class Commercial extends Employe {
     public void setCaAnnuel(Double caAnnuel) {
         this.caAnnuel = caAnnuel;
     }
+
+    @Override
+    public Double getPrimeAnnuelle() {
+        Double primeAnnuelle = Math.ceil(0.05 * caAnnuel);
+
+        if (primeAnnuelle < 500.0 || caAnnuel.equals(null)) {
+            primeAnnuelle = 500.0;
+        }
+        return primeAnnuelle;
+    }
 }
