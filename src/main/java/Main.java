35d3396c0b17b8d06d80de449709882930039bd3
erichuytran <erictran@live.fr>
2020-10-08 11:29:23 +0200

    202 Done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Employe employe1 = new Employe("Eric", "Tran", "12345", LocalDate.parse("1970-01-01"), 500.0);
         Employe employe2 = new Employe("Eric", "Tran", "12345", LocalDate.parse("1970-01-01"), 500.0);
+
         System.out.println(employe1.toString());
         if (employe2.equals(employe1) == true)
             System.out.println("employe2 = employe1");
--- src/main/java/com/ipiecoles/java/java220/Commercial.java
+++ src/main/java/com/ipiecoles/java/java220/Commercial.java
@@ -1,7 +1,17 @@
 package com.ipiecoles.java.java220;
 
 public class Commercial extends Employe {
+    private Double caAnnuel = 0.0;
+
     Commercial() {
         super();
+    }
+
+    public Double getCaAnnuel() {
+        return caAnnuel;
+    }
+
+    public void setCaAnnuel(Double caAnnuel) {
+        this.caAnnuel = caAnnuel;
     }
 }
