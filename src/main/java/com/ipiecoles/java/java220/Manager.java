39a6416dd614bee5cd8ed180c9e2d5278de35ac7
erichuytran <erictran@live.fr>
2020-11-02 16:31:27 +0100

    502 done

--- src/main/java/com/ipiecoles/java/java220/Manager.java
+++ src/main/java/com/ipiecoles/java/java220/Manager.java
@@ -1,7 +1,20 @@
 package com.ipiecoles.java.java220;
 
+import java.util.Collections;
+import java.util.HashSet;
+
 public class Manager extends Employe {
-    
+
+    private HashSet<Technicien> equipe = new HashSet<>();
+
+    public HashSet<Technicien> getEquipe() {
+        return equipe;
+    }
+
+    public void setEquipe(HashSet<Technicien> equipe) {
+        this.equipe = equipe;
+    }
+
     @Override
     public Double getPrimeAnnuelle() {
         return null;
